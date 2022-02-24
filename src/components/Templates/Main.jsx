df4ae93b67afdc39d3efeff49a74dece86ce8f09
이현재 <presentlee@nexon.co.kr>
2022-02-24 10:29:27 +0900

    ENH: 기본 레이아웃 생성

---
+++ src/components/Templates/Main.jsx
@@ -0,0 +1,15 @@
+export default function MainTemplate() {
+  return (
+    <div className="main">
+      {/* <Header/><Form/> */}
+      <div className="content">
+        <div>
+          <h1>Take a hit, let's make a noise</h1>
+        </div>
+        <div>
+          <button className="btn">Minting</button>
+        </div>
+      </div>
+    </div>
+  );
+}
